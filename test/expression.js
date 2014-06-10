224b43b99ca1df8bce03909f5b0ad4c189776b5a
Sergey Chikuyonok <serge.che@gmail.com>
2014-06-10 19:33:10 +0400

    Code layout refactoring: added string stream as dependency

--- test/expression.js
+++ test/expression.js
@@ -1,5 +1,25 @@
 var assert = require('assert');
-var expr = require('../lib/expressionParser');
+var expression = require('../index');
+
+function cleanTokens(tokens) {
+	tokens.forEach(function(t) {
+		delete t.xpathCondition;
+	});
+	return tokens;
+}
+
+function expr(e) {
+	return expression(e).map(function(token) {
+		if (Array.isArray(token)) {
+			cleanTokens(token);
+		}
+		return token;
+	});
+}
+
+function parse(e) {
+	return cleanTokens(expression.parse(e));
+}
 
 describe('Expression Parser', function() {
 	it('should tokenize string', function() {
@@ -11,28 +31,28 @@ describe('Expression Parser', function() {
 	});
 
 	it('should parse simple expression', function() {
-		assert.deepEqual(expr.parse('a'), [{value: 'a'}]);
-		assert.deepEqual(expr.parse('a?'), [{value: 'a', condition: '?'}]);
-		assert.deepEqual(expr.parse('a if b'), [{value: 'a', condition: 'b'}]);
-		assert.deepEqual(expr.parse('a? if b'), [{value: 'a', condition: 'b'}]);
+		assert.deepEqual(parse('a'), [{value: 'a'}]);
+		assert.deepEqual(parse('a?'), [{value: 'a', condition: '?'}]);
+		assert.deepEqual(parse('a if b'), [{value: 'a', condition: 'b'}]);
+		assert.deepEqual(parse('a? if b'), [{value: 'a', condition: 'b'}]);
 	});
 
 	it('should parse lists', function() {
-		assert.deepEqual(expr.parse('a, b'), [{value: 'a'}, {value: 'b'}]);
-		assert.deepEqual(expr.parse('a, "b, c"'), [{value: 'a'}, {value: '"b, c"'}]);
-		assert.deepEqual(expr.parse('a, "b, c", d'), [{value: 'a'}, {value: '"b, c"'}, {value: 'd'}]);
-		assert.deepEqual(expr.parse('a?, b if c'), [{value: 'a', condition: '?'}, {value: 'b', condition: 'c'}]);
-		assert.deepEqual(expr.parse('a, "b, c" if d, e'), [{value: 'a'}, {value: '"b, c"', condition: 'd'}, {value: 'e'}]);
+		assert.deepEqual(parse('a, b'), [{value: 'a'}, {value: 'b'}]);
+		assert.deepEqual(parse('a, "b, c"'), [{value: 'a'}, {value: '"b, c"'}]);
+		assert.deepEqual(parse('a, "b, c", d'), [{value: 'a'}, {value: '"b, c"'}, {value: 'd'}]);
+		assert.deepEqual(parse('a?, b if c'), [{value: 'a', condition: '?'}, {value: 'b', condition: 'c'}]);
+		assert.deepEqual(parse('a, "b, c" if d, e'), [{value: 'a'}, {value: '"b, c"', condition: 'd'}, {value: 'e'}]);
 	});
 
 	it('should parse hashes', function() {
-		assert.deepEqual(expr.parse('a: b'), [{value: 'b', key: 'a'}]);
-		assert.deepEqual(expr.parse('a: b, c: d'), [{value: 'b', key: 'a'}, {value: 'd', key: 'c'}]);
+		assert.deepEqual(parse('a: b'), [{value: 'b', key: 'a'}]);
+		assert.deepEqual(parse('a: b, c: d'), [{value: 'b', key: 'a'}, {value: 'd', key: 'c'}]);
 	});
 
 	it('should throw exceptions on illegal token mix', function() {
 		assert.throws(function() {
-			expr.parse('a: b, c')
+			parse('a: b, c')
 		});
 	});
 });
